6e11e2603de1f017f98eee3d11a218ebb9f04bca
Amardeep T <amardeept@annait.net>
2020-07-31 06:08:35 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       Hello react app
     </div>
   );
